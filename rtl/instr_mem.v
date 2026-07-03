78a0a220df6939cf442d439e61c0c0181c238f95
chunningkao <chunning511@gmail.com>
2026-07-03 11:02:43 -0400

    instr_mem initial completion

---
+++ rtl/instr_mem.v
@@ -0,0 +1,13 @@
+module instr_mem(
+    input [31:0] pc,
+    output [31:0] instr
+);
+    reg [31:0] mem [0:511]; // 2KB instruction space
+    
+    initial begin
+        $readmemh("tb/programs/add_test.hex", mem);
+    end
+
+    assign instr = mem[pc[10:2]]; // 9 bits of PC
+
+endmodule
