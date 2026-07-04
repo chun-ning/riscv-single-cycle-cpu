16ea777260ff8326987e57b451c8e42d270ace96
chunningkao <chunning511@gmail.com>
2026-07-04 19:17:34 -0400

    pc inital ver. completed

---
+++ rtl/pc.v
@@ -0,0 +1,20 @@
+module pc(
+    input clk,
+    input reset,
+    input branch,
+    input jump,
+    input [31:0] imm,
+    output reg [31:0] pc
+);
+
+    always @(posedge clk) begin
+        if (reset) begin
+            pc <= 32'h0;
+        end else if (jump || branch) begin
+            pc <= pc += imm;
+        end else begin
+            pc <= pc += 32'd4;
+        end
+    end
+
+endmodule
